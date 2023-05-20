0d7a8d67a72f7f5c8d55a8680a8115b340ae00bb
ericLemanissier <eric.lemanissier@gmail.com>
2023-05-21 00:13:05 +0200

    bios_print function

--- boot.asm
+++ boot.asm
@@ -1,21 +1,27 @@
-; boot.asm
-   mov ax, 0x07c0
+[ORG 0x7c00]
+   xor ax, ax  ;make it zero
    mov ds, ax
+   cld
 
    mov si, msg
-   cld
-ch_loop:lodsb
-   or al, al ; zero=end of str
-   jz hang   ; get out
-   mov ah, 0x0E
-   mov bh, 0
-   int 0x10
-   jmp ch_loop
+   call bios_print
 
 hang:
    jmp hang
 
 msg   db 'Hello World', 13, 10, 0
+
+bios_print:
+   lodsb
+   or al, al  ;zero=end of str
+   jz done    ;get out
+   mov ah, 0x0E
+   mov bh, 0
+   int 0x10
+   jmp bios_print
+done:
+   ret
+
    times 510-($-$$) db 0
    db 0x55
    db 0xAA
