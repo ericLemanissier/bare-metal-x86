3d1d4f73e8d0c45fc1a129c34935912a969481fd
ericLemanissier <eric.lemanissier@gmail.com>
2023-05-21 18:02:34 +0200

    use c++

--- Makefile
+++ Makefile
@@ -1,3 +1,13 @@
+
+export PREFIX:=${PWD}/opt/cross
+export TARGET:=i686-elf
+export PATH:=$(PREFIX)/bin:$(PATH)
+
 all: boot.asm
-	nasm boot.asm -f bin -o boot.bin
-	qemu-system-i386 -drive format=raw,file=boot.bin
+	i686-elf-as boot.asm -o boot.o
+	i686-elf-g++ -c kernel.cpp -o kernel.o -ffreestanding -O2 -Wall -Wextra -fno-exceptions -fno-rtti
+	i686-elf-gcc -T linker.ld -o myos.bin -ffreestanding -O2 -nostdlib boot.o kernel.o -lgcc
+	qemu-system-i386 -kernel myos.bin
+
+toolchain:
+	cd toolchain && $(MAKE)
--- boot.asm
+++ boot.asm
@@ -1,79 +1,109 @@
-[ORG 0x7c00]      ; add to offsets
-   jmp start
+/* Declare constants for the multiboot header. */
+.set ALIGN,    1<<0             /* align loaded modules on page boundaries */
+.set MEMINFO,  1<<1             /* provide memory map */
+.set FLAGS,    ALIGN | MEMINFO  /* this is the Multiboot 'flag' field */
+.set MAGIC,    0x1BADB002       /* 'magic number' lets bootloader find the header */
+.set CHECKSUM, -(MAGIC + FLAGS) /* checksum of above, to prove we are multiboot */
 
-   %include "print.inc"
+/*
+Declare a multiboot header that marks the program as a kernel. These are magic
+values that are documented in the multiboot standard. The bootloader will
+search for this signature in the first 8 KiB of the kernel file, aligned at a
+32-bit boundary. The signature is in its own section so the header can be
+forced to be within the first 8 KiB of the kernel file.
+*/
+.section .multiboot
+.align 4
+.long MAGIC
+.long FLAGS
+.long CHECKSUM
 
-start:   xor ax, ax   ; make it zero
-   mov ds, ax   ; DS=0
-   mov ss, ax   ; stack starts at 0
-   mov sp, 0x9c00   ; 2000h past code start
+/*
+The multiboot standard does not define the value of the stack pointer register
+(esp) and it is up to the kernel to provide a stack. This allocates room for a
+small stack by creating a symbol at the bottom of it, then allocating 16384
+bytes for it, and finally creating a symbol at the top. The stack grows
+downwards on x86. The stack is in its own section so it can be marked nobits,
+which means the kernel file is smaller because it does not contain an
+uninitialized stack. The stack on x86 must be 16-byte aligned according to the
+System V ABI standard and de-facto extensions. The compiler will assume the
+stack is properly aligned and failure to align the stack will result in
+undefined behavior.
+*/
+.section .bss
+.align 16
+stack_bottom:
+.skip 16384 # 16 KiB
+stack_top:
 
-   cli      ; no interrupt
-   push ds      ; save real mode
+/*
+The linker script specifies _start as the entry point to the kernel and the
+bootloader will jump to this position once the kernel has been loaded. It
+doesn't make sense to return from this function as the bootloader is gone.
+*/
+.section .text
+.global _start
+.type _start, @function
+_start:
+	/*
+	The bootloader has loaded us into 32-bit protected mode on a x86
+	machine. Interrupts are disabled. Paging is disabled. The processor
+	state is as defined in the multiboot standard. The kernel has full
+	control of the CPU. The kernel can only make use of hardware features
+	and any code it provides as part of itself. There's no printf
+	function, unless the kernel provides its own <stdio.h> header and a
+	printf implementation. There are no security restrictions, no
+	safeguards, no debugging mechanisms, only what the kernel provides
+	itself. It has absolute and complete power over the
+	machine.
+	*/
 
-   lgdt [gdtinfo]   ; load gdt register
+	/*
+	To set up a stack, we set the esp register to point to the top of the
+	stack (as it grows downwards on x86 systems). This is necessarily done
+	in assembly as languages such as C cannot function without a stack.
+	*/
+	mov $stack_top, %esp
 
-   mov  eax, cr0   ; switch to pmode by
-   or al,1         ; set pmode bit
-   mov  cr0, eax
+	/*
+	This is a good place to initialize crucial processor state before the
+	high-level kernel is entered. It's best to minimize the early
+	environment where crucial features are offline. Note that the
+	processor is not fully initialized yet: Features such as floating
+	point instructions and instruction set extensions are not initialized
+	yet. The GDT should be loaded here. Paging should be enabled here.
+	C++ features such as global constructors and exceptions will require
+	runtime support to work as well.
+	*/
 
-   mov  bx, 0x08   ; select descriptor 1
-   mov  ds, bx   ; 8h = 1000b
+	/*
+	Enter the high-level kernel. The ABI requires the stack is 16-byte
+	aligned at the time of the call instruction (which afterwards pushes
+	the return pointer of size 4 bytes). The stack was originally 16-byte
+	aligned above and we've pushed a multiple of 16 bytes to the
+	stack since (pushed 0 bytes so far), so the alignment has thus been
+	preserved and the call is well defined.
+	*/
+	call kernel_main
 
-   and al,0xFE     ; back to realmode
-   mov  cr0, eax   ; by toggling bit again
+	/*
+	If the system has nothing more to do, put the computer into an
+	infinite loop. To do that:
+	1) Disable interrupts with cli (clear interrupt enable in eflags).
+	   They are already disabled by the bootloader, so this is not needed.
+	   Mind that you might later enable interrupts and return from
+	   kernel_main (which is sort of nonsensical to do).
+	2) Wait for the next interrupt to arrive with hlt (halt instruction).
+	   Since they are disabled, this will lock up the computer.
+	3) Jump to the hlt instruction if it ever wakes up due to a
+	   non-maskable interrupt occurring or due to system management mode.
+	*/
+	cli
+1:	hlt
+	jmp 1b
 
-   pop ds      ; get back old segment
-   sti
-
-   mov bx, 0x0f01   ; attrib/char of smiley
-   mov eax, 0x0b8000 ; note 32 bit offset
-   mov word [ds:eax], bx
-   mov ds, ax   ; DS=0
-   mov ss, ax   ; stack starts at 0
-   mov sp, 0x9c00   ; 200h past code start
-
-   mov ax, 0xb800   ; text video memory
-   mov es, ax
-
-   cli      ;no interruptions
-   mov bx, 0x09   ;hardware interrupt #
-   shl bx, 2   ;multiply by 4
-   xor ax, ax
-   mov gs, ax   ;start of memory
-   mov [gs:bx], word keyhandler
-   mov [gs:bx+2], ds ; segment
-   sti
-
-   jmp $      ; loop forever
-
-keyhandler:
-   in al, 0x60   ; get key data
-   mov bl, al   ; save it
-   mov byte [port60], al
-
-   in al, 0x61   ; keybrd control
-   mov ah, al
-   or al, 0x80   ; disable bit 7
-   out 0x61, al   ; send it back
-   xchg ah, al   ; get original
-   out 0x61, al   ; send that back
-
-   mov al, 0x20   ; End of Interrupt
-   out 0x20, al   ;
-
-   and bl, 0x80   ; key released
-   jnz done   ; don't repeat
-
-   mov ax, [port60]
-   mov  word [reg16], ax
-   call printreg16
-
-done:
-   iret
-
-port60   dw 0
-
-   times 510-($-$$) db 0  ; fill sector w/ 0's
-   dw 0xAA55        ; req'd by some BIOSes
-;==========================================
+/*
+Set the size of the _start symbol to the current location '.' minus its start.
+This is useful when debugging or when you implement call tracing.
+*/
+.size _start, . - _start
