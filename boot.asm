30a5a5e1e6e23824a5d8037794f39b2ba34a7dfa
ericLemanissier <eric.lemanissier@gmail.com>
2023-05-21 17:24:05 +0200

    add toolchain

--- boot.asm
+++ boot.asm
@@ -6,6 +6,31 @@
 start:   xor ax, ax   ; make it zero
    mov ds, ax   ; DS=0
    mov ss, ax   ; stack starts at 0
+   mov sp, 0x9c00   ; 2000h past code start
+
+   cli      ; no interrupt
+   push ds      ; save real mode
+
+   lgdt [gdtinfo]   ; load gdt register
+
+   mov  eax, cr0   ; switch to pmode by
+   or al,1         ; set pmode bit
+   mov  cr0, eax
+
+   mov  bx, 0x08   ; select descriptor 1
+   mov  ds, bx   ; 8h = 1000b
+
+   and al,0xFE     ; back to realmode
+   mov  cr0, eax   ; by toggling bit again
+
+   pop ds      ; get back old segment
+   sti
+
+   mov bx, 0x0f01   ; attrib/char of smiley
+   mov eax, 0x0b8000 ; note 32 bit offset
+   mov word [ds:eax], bx
+   mov ds, ax   ; DS=0
+   mov ss, ax   ; stack starts at 0
    mov sp, 0x9c00   ; 200h past code start
 
    mov ax, 0xb800   ; text video memory
