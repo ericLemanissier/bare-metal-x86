a84876b2536917d43be3d23b3bf458f5b8ac1f7b
ericLemanissier <eric.lemanissier@gmail.com>
2023-05-22 17:10:08 +0200

    plot rects

--- Makefile
+++ Makefile
@@ -1,12 +1,26 @@
 
+CFLAGS?=-O2 -g
+CPPFLAGS?=
+LDFLAGS?=
+LIBS?=
+
+AS=i686-elf-as
+CC=i686-elf-gcc
+CXX=i686-elf-g++
+
+CFLAGS:=$(CFLAGS) -D__is_kernel -Iinclude -ffreestanding -Wall -Wextra -fno-exceptions -fno-rtti
+CPPFLAGS:=$(CPPFLAGS) -D__is_kernel -Iinclude -ffreestanding -Wall -Wextra -fno-exceptions -fno-rtti
+LDFLAGS:=$(LDFLAGS) -ffreestanding -O2
+LIBS:=$(LIBS) -nostdlib -lgcc
+
 export PREFIX:=${PWD}/opt/cross
 export TARGET:=i686-elf
 export PATH:=$(PREFIX)/bin:$(PATH)
 
 all: boot.asm
-	i686-elf-as boot.asm -o boot.o
-	i686-elf-g++ -c kernel.cpp -o kernel.o -ffreestanding -O2 -Wall -Wextra -fno-exceptions -fno-rtti
-	i686-elf-gcc -T linker.ld -o myos.bin -ffreestanding -O2 -nostdlib boot.o kernel.o -lgcc
+	$(AS) boot.asm -o boot.o
+	$(CXX) -c kernel.cpp -o kernel.o $(CPPFLAGS)
+	$(CXX) -T linker.ld -o myos.bin $(LDFLAGS) $(LIBS) boot.o kernel.o
 	qemu-system-i386 -kernel myos.bin
 
 toolchain:
--- kernel.cpp
+++ kernel.cpp
@@ -34,12 +34,12 @@ enum vga_color {
 	VGA_COLOR_WHITE = 15,
 };
 
-static inline uint8_t vga_entry_color(enum vga_color fg, enum vga_color bg)
+static constexpr uint8_t vga_entry_color(enum vga_color fg, enum vga_color bg)
 {
 	return fg | bg << 4;
 }
 
-static inline uint16_t vga_entry(unsigned char uc, uint8_t color)
+static constexpr uint16_t vga_entry(unsigned char uc, uint8_t color)
 {
 	return (uint16_t) uc | (uint16_t) color << 8;
 }
@@ -52,8 +52,56 @@ size_t strlen(const char* str)
 	return len;
 }
 
-static const size_t VGA_WIDTH = 80;
-static const size_t VGA_HEIGHT = 25;
+struct Point
+{
+	size_t x{};
+	size_t y{};
+};
+
+struct Size
+{
+	size_t w{};
+	size_t h{};
+};
+
+struct Rectangle
+{
+	Point m_top_left{};
+	Size size{};
+
+	constexpr Point top_left() const
+	{
+		return m_top_left;
+	}
+
+	constexpr Point top_right() const
+	{
+		return {m_top_left.x + width() - 1, m_top_left.y};
+	}
+
+	constexpr Point bottom_left() const
+	{
+		return {m_top_left.x, m_top_left.y + height() - 1};
+	}
+
+	constexpr Point bottom_right() const
+	{
+		return {m_top_left.x + width() - 1, m_top_left.y + height() - 1};
+	}
+
+	constexpr size_t width() const
+	{
+		return size.w;
+	}
+
+	constexpr size_t height() const
+	{
+		return size.h;
+	}
+};
+
+static constexpr size_t VGA_WIDTH = 80;
+static constexpr size_t VGA_HEIGHT = 25;
 
 size_t terminal_row;
 size_t terminal_column;
@@ -85,13 +133,34 @@ void terminal_putentryat(char c, uint8_t color, size_t x, size_t y)
 	terminal_buffer[index] = vga_entry(c, color);
 }
 
+void terminal_newline()
+{
+	terminal_column = 0;
+	if (++terminal_row == VGA_HEIGHT)
+	{
+		for (size_t y = 0; y < VGA_HEIGHT - 1; y++) {
+			for (size_t x = 0; x < VGA_WIDTH; x++) {
+				terminal_buffer[y * VGA_WIDTH + x] = terminal_buffer[(y + 1) * VGA_WIDTH + x];
+			}
+		}
+		terminal_row--;
+		for (size_t x = 0; x < VGA_WIDTH; x++)
+			terminal_buffer[terminal_row * VGA_WIDTH + x] = 0;
+	}
+}
+
+
 void terminal_putchar(char c)
 {
-	terminal_putentryat(c, terminal_color, terminal_column, terminal_row);
-	if (++terminal_column == VGA_WIDTH) {
-		terminal_column = 0;
-		if (++terminal_row == VGA_HEIGHT)
-			terminal_row = 0;
+	if(c == '\n')
+	{
+		terminal_newline();
+	}
+	else {
+		terminal_putentryat(c, terminal_color, terminal_column, terminal_row);
+		if (++terminal_column == VGA_WIDTH) {
+			terminal_newline();
+		}
 	}
 }
 
@@ -106,6 +175,59 @@ void terminal_writestring(const char* data)
 	terminal_write(data, strlen(data));
 }
 
+void terminal_write_hex_nibble(uint8_t n)
+{
+	if(n > 9)
+		terminal_putchar('A' + n - 10);
+	else
+		terminal_putchar('0' + n);
+}
+
+void terminal_writehex(size_t h)
+{
+	if(const auto high = h / 16)
+		terminal_writehex(high);
+	terminal_write_hex_nibble(h % 16);
+}
+
+enum class Outline
+{
+	Single,
+	Double
+};
+
+
+void terminal_putentryat(char c, size_t x, size_t y, uint8_t color = terminal_color)
+{
+	terminal_putentryat(c, color, x, y);
+}
+
+void terminal_putentryat(char c, Point p, uint8_t color = terminal_color)
+{
+	terminal_putentryat(c, color, p.x, p.y);
+}
+
+void terminal_rect(const Rectangle &rect, Outline o = Outline::Single)
+{
+	terminal_putentryat(o == Outline::Single ? 0xDA : 0xC9, rect.top_left());
+	terminal_putentryat(o == Outline::Single ? 0xC0 : 0xC8, rect.bottom_left());
+	terminal_putentryat(o == Outline::Single ? 0xBF : 0xBB, rect.top_right());
+	terminal_putentryat(o == Outline::Single ? 0xD9 : 0xBC, rect.bottom_right());
+
+	for(size_t y = 1; y + 1 < rect.height(); y++)
+	{
+		terminal_putentryat(o == Outline::Single ? 0xB3 : 0xBA, rect.top_left().x, rect.top_left().y + y);
+		terminal_putentryat(o == Outline::Single ? 0xB3 : 0xBA, rect.top_right().x, rect.top_right().y + y);
+	}
+
+	for(size_t x = 1; x + 1 < rect.width(); x++)
+	{
+		terminal_putentryat(o == Outline::Single ? 0xC4 : 0xCD, rect.top_left().x + x, rect.top_left().y);
+		terminal_putentryat(o == Outline::Single ? 0xC4 : 0xCD, rect.bottom_left().x + x, rect.bottom_left().y);
+	}
+
+}
+
 void kernel_main(void)
 {
 	/* Initialize terminal interface */
@@ -113,4 +235,26 @@ void kernel_main(void)
 
 	/* Newline support is left as an exercise. */
 	terminal_writestring("Hello, kernel World!\n");
+	terminal_setcolor(VGA_COLOR_BLUE | VGA_COLOR_WHITE << 4);
+	terminal_writestring("how are you doing ?\n I'm doing fine\n");
+	terminal_setcolor(VGA_COLOR_LIGHT_GREY | VGA_COLOR_BLACK << 4);
+	/*const auto n = 9;
+	for(size_t i = n * VGA_HEIGHT; i < (n + 1) * VGA_HEIGHT; i++)
+	{
+		terminal_writestring("0x");
+		terminal_writehex(i);
+		terminal_putchar(':');
+		terminal_putchar(i);
+		terminal_putchar('\n');
+	}*/
+	//terminal_writestring("0x");terminal_writehex(0);
+	terminal_writestring("0x");terminal_writehex(0);
+	terminal_writestring(" 0x");terminal_writehex(1);
+	terminal_writestring(" 0x");terminal_writehex(255);
+	terminal_writestring(" 0x");terminal_writehex(256);
+	terminal_writestring(" 0x");terminal_writehex(65535);
+	terminal_writestring(" 0x");terminal_writehex(65536);
+	terminal_rect(Rectangle{Point{9, 9}, Size{12,10}}, Outline::Single);
+	terminal_rect(Rectangle{Point{10, 10}, Size{5,8}}, Outline::Double);
+	terminal_rect(Rectangle{Point{15, 10}, Size{5,3}}, Outline::Single);
 }
